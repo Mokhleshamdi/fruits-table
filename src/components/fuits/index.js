2b0ee55965aec5ac4ab5ad7ec422e9e204e06d2d
Mokhles <mokhles.hamdi@digitarm.com>
2022-12-19 14:33:43 +0100

    first commit

---
+++ src/components/fuits/index.js
@@ -0,0 +1,64 @@
+import React, { Fragment, useState } from 'react'
+import { data } from '../../fakeData'
+import './style.css';
+
+export default function Fruits() {
+    const [fruits, setFruits] = useState(data)
+    //  ASCENDING 
+    const sortAscending = (attr) => setFruits([...data].sort((a, b) =>
+        attr == 'byName' ? a.name > b.name ? 1 : -1 : attr == 'byDesc' ? a.name > b.name ? 1 : -1 : attr == 'byPrice' ? a.name > b.name ? 1 : -1 : a.attr > b.attr ? 1 : -1,
+    ))
+
+    // DESCENDING
+    const sortDescending = (attr) => setFruits([...data].sort((a, b) =>
+        attr == 'byName' ? a.name > b.name ? -1 : 1 : attr == 'byDesc' ? a.name > b.name ? -1 : 1 : attr == 'byPrice' ? a.name > b.name ? -1 : 1 : a.attr > b.attr ? 1 : -1,
+    ))
+    return (
+        <Fragment>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>
+                            <div className='row'>
+                                <h4>Name</h4>
+                                <div className='column' style={{ display: 'flex', justifyContent: "flex-end" }}>
+                                    <button onClick={() => sortAscending('byName')}><div style={{ color: "#fff" }}>˄</div></button>
+                                    <button onClick={() => sortDescending('byName')} > <div style={{ color: "#fff" }}>˅</div></button>
+                                </div>
+                            </div>
+                        </th>
+                        <th><div className='row'>
+                            <h4>Description</h4>
+                            <div className='column' style={{ display: 'flex', justifyContent: "flex-end" }}>
+                                <button onClick={() => sortAscending('byDesc')} > <div style={{ color: "#fff" }}>˄</div></button>
+                                <button onClick={() => sortDescending('byDesc')} > <div style={{ color: "#fff" }}>˅</div></button>
+                            </div>
+                        </div></th>
+                        <th><div className='row'>
+                            <h4>price</h4>
+                            <div className='column' style={{ display: 'flex', justifyContent: "flex-end" }}>
+                                <button onClick={() => sortAscending('byPrice')}><div style={{ color: "#fff" }}>˄</div></button>
+                                <button onClick={() => sortDescending('byPrice')} > <div style={{ color: "#fff" }}>˅</div></button>
+                            </div>
+                        </div></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fruits.map((el, index) => {
+                        return (
+                            <tr key={index}>
+                                <th>{el.id}</th>
+                                <th>
+                                    {el.name}
+
+                                </th>
+                                <th>{el.description}</th>
+                                <th>{el.price}</th>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </Fragment>)
+}
---
+++ src/fakeData/index.js
@@ -0,0 +1,77 @@
+export const data = [
+    {
+        id: 1,
+        name: "Apple",
+        description: "A red fruit ",
+        price: 0.99
+    },
+    
+    {
+        id: 2,
+        name: "Carrot ",
+        description: "A orange vegetable ",
+        price: 1.69
+    },
+    {
+        id: 3,
+        name: "Banana ",
+        description: "A yellow fruit ",
+        price: 0.79
+    },
+    {
+        id: 4,
+        name: "Orange",
+        description: "An orange fruit ",
+        price: 1.99
+    },
+    {
+        id: 5,
+        name: "Manga",
+        description: "A yellow fruit ",
+        price: 1.79
+    },
+    {
+        id: 6,
+        name: "Strobary",
+        description: "A prink vegetable ",
+        price: 0.69
+    },
+    {
+        id: 7,
+        name: "Apple",
+        description: "A red fruit ",
+        price: 0.99
+    },
+    
+    {
+        id: 8,
+        name: "Carrot ",
+        description: "A orange vegetable ",
+        price: 1.69
+    },
+    {
+        id: 9,
+        name: "Banana ",
+        description: "A yellow fruit ",
+        price: 0.79
+    },
+    {
+        id: 10,
+        name: "Orange",
+        description: "An orange fruit ",
+        price: 1.99
+    },
+    {
+        id: 11,
+        name: "Manga",
+        description: "A yellow fruit ",
+        price: 1.79
+    },
+    {
+        id: 12,
+        name: "Strobary",
+        description: "A prink vegetable ",
+        price: 0.69
+    }
+    
+]
